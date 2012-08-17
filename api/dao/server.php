1ac86eb5e49db471a82f6b194041b768f05de95e
Jeff Standen <jeff@webgroupmedia.com>
2012-08-17 14:10:18 -0700

    6.1 compatibility

--- api/dao/server.php
+++ api/dao/server.php
@@ -432,15 +432,21 @@ class DAO_Server extends C4_ORMHelper {
 
 		// Translate virtual fields
 		
+		$args = array(
+			'join_sql' => $join_sql,
+			'where_sql' => $where_sql,
+			'has_multiple_values' => $has_multiple_values
+		);
+		
 		array_walk_recursive(
 			$params,
 			array('DAO_Server', '_translateVirtualParameters'),
-			array(
-				'join_sql' => &$join_sql,
-				'where_sql' => &$where_sql,
-				'has_multiple_values' => &$has_multiple_values
-			)
+			&$args
 		);
+		
+		$join_sql = $args['join_sql'];
+		$where_sql = $args['where_sql'];
+		$has_multiple_values = $args['has_multiple_values'];
 		
 		$result = array(
 			'primary_table' => 'server',
