a2fbbe632feeb4baad3a24634dd25b5ce9945049
Jeff Standen <jeff@webgroupmedia.com>
2014-05-25 12:09:37 -0700

    6.7.x compatibility

--- api/events/abstract_event_server.php
+++ api/events/abstract_event_server.php
@@ -86,6 +86,7 @@ abstract class AbstractEvent_Server extends Extension_DevblocksEvent {
 			'server_watchers' => array(
 				'label' => 'Server watchers',
 				'context' => CerberusContexts::CONTEXT_WORKER,
+				'is_multiple' => true,
 			),
 		);
 		
@@ -112,14 +113,14 @@ abstract class AbstractEvent_Server extends Extension_DevblocksEvent {
 		return $conditions;
 	}
 	
-	function renderConditionExtension($token, $trigger, $params=array(), $seq=null) {
+	function renderConditionExtension($token, $as_token, $trigger, $params=array(), $seq=null) {
 		$tpl = DevblocksPlatform::getTemplateService();
 		$tpl->assign('params', $params);
 
 		if(!is_null($seq))
 			$tpl->assign('namePrefix','condition'.$seq);
 		
-		switch($token) {
+		switch($as_token) {
 			case 'server_link':
 				$contexts = Extension_DevblocksContext::getAll(false);
 				$tpl->assign('contexts', $contexts);
@@ -135,10 +136,10 @@ abstract class AbstractEvent_Server extends Extension_DevblocksEvent {
 		$tpl->clearAssign('params');
 	}
 	
-	function runConditionExtension($token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
+	function runConditionExtension($token, $as_token, $trigger, $params, DevblocksDictionaryDelegate $dict) {
 		$pass = true;
 		
-		switch($token) {
+		switch($as_token) {
 			case 'server_link':
 				$not = (substr($params['oper'],0,1) == '!');
 				$oper = ltrim($params['oper'],'!');
@@ -146,7 +147,7 @@ abstract class AbstractEvent_Server extends Extension_DevblocksEvent {
 				$from_context = null;
 				$from_context_id = null;
 				
-				switch($token) {
+				switch($as_token) {
 					case 'server_link':
 						$from_context = CerberusContexts::CONTEXT_SERVER;
 						@$from_context_id = $dict->server_id;
