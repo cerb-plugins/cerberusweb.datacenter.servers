1765b5171db72bbea21f492328457a2f0ff3b40f
Jeff Standen <jeff@webgroupmedia.com>
2016-01-04 16:57:31 -0800

    * [Plugins/Servers/VAs] Implemented a 'Create server' action in Virtual Attendant behaviors.

--- api/App.php
+++ api/App.php
@@ -271,6 +271,131 @@ class Page_Datacenter extends CerberusPageExtension {
 	}
 };
 
+if(class_exists('Extension_DevblocksEventAction')):
+class VaAction_CreateServer extends Extension_DevblocksEventAction {
+	function render(Extension_DevblocksEvent $event, Model_TriggerEvent $trigger, $params=array(), $seq=null) {
+		$tpl = DevblocksPlatform::getTemplateService();
+		$tpl->assign('params', $params);
+		
+		if(!is_null($seq))
+			$tpl->assign('namePrefix', 'action'.$seq);
+		
+		$event = $trigger->getEvent();
+		$values_to_contexts = $event->getValuesContexts($trigger);
+		$tpl->assign('values_to_contexts', $values_to_contexts);
+		
+		// Custom fields
+		DevblocksEventHelper::renderActionCreateRecordSetCustomFields(CerberusContexts::CONTEXT_SERVER, $tpl);
+		
+		// Template
+		$tpl->display('devblocks:cerberusweb.datacenter.servers::events/action_create_server.tpl');
+	}
+	
+	function simulate($token, Model_TriggerEvent $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+
+		$out = null;
+		
+		@$name = $tpl_builder->build($params['name'], $dict);
+		
+		@$notify_worker_ids = DevblocksPlatform::importVar($params['notify_worker_id'],'array',array());
+		$notify_worker_ids = DevblocksEventHelper::mergeWorkerVars($notify_worker_ids, $dict);
+		
+		if(empty($name))
+			return "[ERROR] Name is required.";
+		
+		// Check dupes
+		if(false != ($server = DAO_Server::getByName($name))) {
+			return sprintf("[ERROR] Name must be unique. A server named '%s' already exists.", $name);
+		}
+		
+		$comment = $tpl_builder->build($params['comment'], $dict);
+		
+		$out = sprintf(">>> Creating server: %s\n", $name);
+		
+		// Custom fields
+		$out .= DevblocksEventHelper::simulateActionCreateRecordSetCustomFields($params, $dict);
+		
+		$out .= "\n";
+		
+		// Comment content
+		if(!empty($comment)) {
+			$out .= sprintf(">>> Writing comment on server\n\n".
+				"%s\n\n",
+				$comment
+			);
+			
+			if(!empty($notify_worker_ids) && is_array($notify_worker_ids)) {
+				$out .= ">>> Notifying\n";
+				foreach($notify_worker_ids as $worker_id) {
+					if(null != ($worker = DAO_Worker::get($worker_id))) {
+						$out .= ' * ' . $worker->getName() . "\n";
+					}
+				}
+				$out .= "\n";
+			}
+		}
+		
+		// Set object variable
+		$out .= DevblocksEventHelper::simulateActionCreateRecordSetVariable($params, $dict);
+		
+		// Run in simulator
+		@$run_in_simulator = !empty($params['run_in_simulator']);
+		if($run_in_simulator) {
+			$this->run($token, $trigger, $params, $dict);
+		}
+		
+		return $out;
+	}
+	
+	function run($token, Model_TriggerEvent $trigger, $params, DevblocksDictionaryDelegate $dict) {
+		$tpl_builder = DevblocksPlatform::getTemplateBuilder();
+		
+		@$name = $tpl_builder->build($params['name'], $dict);
+
+		@$notify_worker_ids = DevblocksPlatform::importVar($params['notify_worker_id'],'array',array());
+		$notify_worker_ids = DevblocksEventHelper::mergeWorkerVars($notify_worker_ids, $dict);
+		
+		$comment = $tpl_builder->build($params['comment'], $dict);
+		
+		if(empty($name))
+			return;
+		
+		// Dupe check
+		if(false != ($server = DAO_Server::getByName($name))) {
+			return;
+		}
+		
+		$fields = array(
+			DAO_Server::NAME => $name,
+		);
+			
+		if(false == ($server_id = DAO_Server::create($fields)))
+			return;
+		
+		// Custom fields
+		DevblocksEventHelper::runActionCreateRecordSetCustomFields(CerberusContexts::CONTEXT_SERVER, $server_id, $params, $dict);
+		
+		// Comment content
+		if(!empty($comment)) {
+			$fields = array(
+				DAO_Comment::OWNER_CONTEXT => CerberusContexts::CONTEXT_VIRTUAL_ATTENDANT,
+				DAO_Comment::OWNER_CONTEXT_ID => $trigger->virtual_attendant_id,
+				DAO_Comment::COMMENT => $comment,
+				DAO_Comment::CONTEXT => CerberusContexts::CONTEXT_SERVER,
+				DAO_Comment::CONTEXT_ID => $server_id,
+				DAO_Comment::CREATED => time(),
+			);
+			DAO_Comment::create($fields, $notify_worker_ids);
+		}
+
+		// Set object variable
+		DevblocksEventHelper::runActionCreateRecordSetVariable(CerberusContexts::CONTEXT_SERVER, $server_id, $params, $dict);
+	}
+	
+};
+endif;
+
 if (class_exists('DevblocksEventListenerExtension')):
 class EventListener_Datacenter extends DevblocksEventListenerExtension {
 	/**
