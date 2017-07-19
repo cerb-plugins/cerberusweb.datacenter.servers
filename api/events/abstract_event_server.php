499da7fe38f45814fdbd64f979c40f300ca90470
Jeff Standen <jeff@webgroupmedia.com>
2017-07-19 01:28:30 -0700

    8.0 compatibility

--- api/events/abstract_event_server.php
+++ api/events/abstract_event_server.php
@@ -100,7 +100,7 @@ abstract class AbstractEvent_Server extends Extension_DevblocksEvent {
 				'context' => CerberusContexts::CONTEXT_BEHAVIOR,
 			),
 			'behavior_bot_id' => array(
-				'label' => 'Behavior',
+				'label' => 'Bot',
 				'context' => CerberusContexts::CONTEXT_BOT,
 			),
 			'server_id' => array(
