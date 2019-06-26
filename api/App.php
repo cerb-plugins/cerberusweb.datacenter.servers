2974cda527a0b251d5ebed773b2c26301ef93072
Jeff Standen <jeff@webgroupmedia.com>
2019-06-26 15:32:13 -0700

    9.3.2 compatibility

--- api/App.php
+++ api/App.php
@@ -82,6 +82,15 @@ class Page_Datacenter extends CerberusPageExtension {
 };
 
 class VaAction_CreateServer extends Extension_DevblocksEventAction {
+	static function getMeta() {
+		return [
+			'notes' => 'Use [Record create](/docs/bots/events/actions/core.bot.action.record.create/) instead.',
+			'deprecated' => true,
+			'params' => [
+			],
+		];
+	}
+	
 	function render(Extension_DevblocksEvent $event, Model_TriggerEvent $trigger, $params=array(), $seq=null) {
 		$tpl = DevblocksPlatform::services()->template();
 		$tpl->assign('params', $params);
